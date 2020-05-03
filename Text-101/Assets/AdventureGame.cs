272c856a7c6c5c24964a25ccd1b1ec09e69ca700
ThreeHDM <juanbilardi@gmail.com>
2020-05-03 10:47:33 -0300

    Create GameObject vacio y update textComponent para mostrar texto programáticamente.

---
+++ Text-101/Assets/AdventureGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Agregamos este namespace para poder usar Text.
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    //SerializeField permite tener esto disponible en nuestro inspector, así Unity solo sugiere objetos de tipo texto
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Accedemos a la prop text del textcomponent
+        textComponent.text = "En el año 2304 el vapor tomó la tierra";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
