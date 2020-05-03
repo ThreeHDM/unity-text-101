f690009fac45d188c15d9b87e0b057dc760d85e3
ThreeHDM <juanbilardi@gmail.com>
2020-05-03 11:35:25 -0300

    Unity Scriptable Objects: Creación y configuración de un ScriptableObject de tipo texto. También modificación de la interfaz.

---
+++ Text-101/Assets/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Creamos un menu con el nombre de la clase. Así aparece en el menú de assets de Unity para ser creado 
+[CreateAssetMenu(menuName = "State")]
+//Cambiamos MonoBehaviour a ScriptableObject en esta clase
+public class State : ScriptableObject
+{
+    //SerializeField permite que aparezca como recurso
+    //TextArea() crea un area de texto en la interfaz de Unity del Scriptable Obj
+    [TextArea(10,14)][SerializeField] string storyText;
+
+}
